631e401558baa47fe56973e92b597e56651d41f9
Phoenix Banana <kian@chateaupoulin.com>
2024-12-08 21:37:40 -0800

    Updated script.js & index.html

--- script.js
+++ script.js
@@ -4,53 +4,6 @@
     const checkButton = document.getElementById('checkButton');
     const resultDisplay = document.getElementById('resultDisplay');
     const guessDisplay = document.getElementById('guessDisplay');
-    let points = 0;
-  let cps = 0;
-  let cpc = 1;
-  let pointsDisplay = document.getElementById("pointsDisplay");
-  let cpsDisplay = document.getElementById("cpsDisplay");
-  let cpcDisplay = document.getElementById("cpcDisplay");
-  let cpsCost = 10;
-  let cpcCost = 5;
-  let cpsCostDisplay = document.getElementById("cpsCostDisplay");
-  let cpcCostDisplay = document.getElementById("cpcCostDisplay");
-  const cpsButton = document.getElementById("cpsButton");
-  const clickButton = document.getElementById("clickButton");
-  const cpcButton = document.getElementById("cpcButton");
-
-  cpsButton.addEventListener('click', () => {
-  if (points >= cpsCost) {
-    cps++;
-    points = points - cpsCost;
-    cpsCost = cpsCost * 2;
-    cpsDisplay.innerHTML = "Points per second: " + cps;
-    cpsCostDisplay.innerHTML = "Buying more PPS will cost " + cpsCost + " points.";
-  } else {
-    console.log("Not enough points. CPS maintained.")
-  }
-});
-
-cpcButton.addEventListener('click', () => {
-  if (points >= cpcCost) {
-    cpc++;
-    points = points - cpcCost;
-    cpcCost = cpcCost + 5;
-    cpcDisplay.innerHTML = "Points per click: " + cpc;
-    cpcCostDisplay.innerHTML = "Buying PPC will cost " + cpcCost + " points.";
-  } else {
-    console.log("Not enough points. CPC maintained.")
-  }
-})
-
-clickButton.addEventListener('click', () => {
-  points = points + cpc;
-  pointsDisplay.innerHTML = "Points: " + points;
-});
-
-setInterval(() => {
-  points += cps;
-  pointsDisplay.innerHTML = "Points: " + points;
-}, 1000);
 
   // Precomputed SHA-256 hash for the answer (lowercase)
   // You can generate this beforehand using a trusted tool.
